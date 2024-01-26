b8d433e28f28d52c012c32d0dc12e7009c3e9ff0
Daniel Ortiz <danielortiz08@gmail.com>
2024-01-25 23:09:56 -0500

    Create database relationships and setup tanstack

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -35,7 +35,10 @@ const SignupForm = () => {
     // 2. Define a submit handler.
     async function onSubmit(values: z.infer<typeof SignupValidator>) {
       const newUser = await createUserAccount(values);
-      console.log(newUser)
+    
+      if(!newUser) {
+        return;
+      }
     }
 
   return (
